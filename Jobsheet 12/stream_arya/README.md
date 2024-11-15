d0c3df4f3d7c0bc732a3ae3faa17594c37e813d3
Arya Chandra <aryachandrak14@gmail.com>
2024-11-15 09:30:55 +0700

    W12: Jawaban Soal 6

--- Jobsheet 12/stream_arya/README.md
+++ Jobsheet 12/stream_arya/README.md
@@ -3,3 +3,7 @@
 Tampilan hasil praktikum 1
 
 ![Hasil Praktikum 1](assets/Praktikum1.gif)
+
+Tampilan hasil praktikum 2
+
+![Hasil Praktikum 2](assets/Praktikum2.gif)
--- Jobsheet 12/stream_arya/lib/main.dart
+++ Jobsheet 12/stream_arya/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'stream.dart';
+import 'dart:async';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -28,13 +30,16 @@ class StreamHomePage extends StatefulWidget {
 class _StreamHomePageState extends State<StreamHomePage> {
   Color bgColor = Colors.blueGrey;
   late ColorStream colorStream;
+  int lastNumber = 0;
+  late StreamController numberStreamController;
+  late NumberStream numberStream;
 
-  //Soal 5
-  //listen: Asinkron secara paralel; melanjutkan eksekusi program tanpa menunggu
-  //stream selesai.
-  //await for: Asinkron secara berurutan; menunggu setiap elemen
-  //selesai diproses sebelum melanjutkan ke elemen berikutnya atau kode
-  //berikutnya.
+  /*Soal 5
+  listen: Asinkron secara paralel; melanjutkan eksekusi program tanpa menunggu
+  stream selesai.
+  await for: Asinkron secara berurutan; menunggu setiap elemen
+  selesai diproses sebelum melanjutkan ke elemen berikutnya atau kode
+  berikutnya.*/
   void changeColor() async {
     colorStream.getColors().listen((eventColor) {
       setState(() {
@@ -49,22 +54,66 @@ class _StreamHomePageState extends State<StreamHomePage> {
     // }
   }
 
+  /* Soal 6 langkah 8
+  Kode berikut menginisialisasi stream (numberStream) dan menambahkan
+  pendengar untuk memproses data yang masuk. Setiap kali event diterima, state
+  diperbarui melalui setState() untuk menyimpan nilai terakhir (lastNumber).
+  super.initState() memastikan inisialisasi awal State berjalan sesuai siklus
+  hidup widget Flutter.*/
   @override
   void initState() {
+    numberStream = NumberStream();
+    numberStreamController = numberStream.controller;
+    Stream stream = numberStreamController.stream;
+    stream.listen((event) {
+      setState(() {
+        lastNumber = event;
+      });
+    });
     super.initState();
-    colorStream = ColorStream();
-    changeColor();
+
+    //void initState() {
+    //  super.initState();
+    //  colorStream = ColorStream();
+    //  changeColor();
+    //}
+  }
+
+  @override
+  void dispose() {
+    numberStreamController.close();
+    super.dispose();
+  }
+
+  /* soal 6 langkah 10
+  Kode berikut menghasilkan angka acak antara 0 hingga 9 menggunakan
+  Random().nextInt(10), kemudian menambahkannya ke stream dengan memanggil
+  numberStream.addNumberToSink(myNum), yang mengalirkan angka tersebut ke sink
+  dari stream untuk diproses lebih lanjut. */
+  void addRandomNumber() {
+    Random random = Random();
+    int myNum = random.nextInt(10);
+    numberStream.addNumberToSink(myNum);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Stream Arya'),
-      ),
-      body: Container(
-        decoration: BoxDecoration(color: bgColor),
-      ),
-    );
+        appBar: AppBar(
+          title: Text('Stream Arya'),
+        ),
+        body: SizedBox(
+          width: double.infinity,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(lastNumber.toString()),
+              ElevatedButton(
+                  onPressed: () => addRandomNumber(),
+                  child: Text('New Random Number'))
+            ],
+          ),
+        ));
   }
 }
