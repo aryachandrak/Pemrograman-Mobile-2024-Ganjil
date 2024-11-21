66fa512d6b679d42e5dc0c3f4db02d07e6593c53
Arya Chandra <aryachandrak14@gmail.com>
2024-11-21 17:59:31 +0700

    W12: Jawaban Soal 8

--- Jobsheet 12/stream_arya/lib/main.dart
+++ Jobsheet 12/stream_arya/lib/main.dart
@@ -28,6 +28,7 @@ class StreamHomePage extends StatefulWidget {
 }
 
 class _StreamHomePageState extends State<StreamHomePage> {
+  late StreamTransformer transformer;
   Color bgColor = Colors.blueGrey;
   late ColorStream colorStream;
   int lastNumber = 0;
@@ -50,10 +51,18 @@ class _StreamHomePageState extends State<StreamHomePage> {
 
   @override
   void initState() {
+    transformer = StreamTransformer<int, int>.fromHandlers(
+        handleData: (value, sink) {
+          sink.add(value * 10);
+        },
+        handleError: (error, trace, sink) {
+          sink.add(-1);
+        },
+        handleDone: (sink) => sink.close());
     numberStream = NumberStream();
     numberStreamController = numberStream.controller;
     Stream stream = numberStreamController.stream;
-    stream.listen((event) {
+    stream.transform(transformer).listen((event) {
       setState(() {
         lastNumber = event;
       });
