df6d5d9ff8263ba58c44faff5531a0c03e3f5782
Arya Chandra <aryachandrak14@gmail.com>
2024-11-29 08:24:51 +0700

    Praktikum 1 Done

---
+++ Jobseet 14/pizza_api_arya/android/app/src/main/kotlin/com/example/pizza_api_arya/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.pizza_api_arya
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Jobseet 14/pizza_api_arya/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:pizza_api_arya/pizza.dart';
+import 'httphelper.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Pizza Api Arya',
+      home: Homepage(),
+    );
+  }
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  Future<List<Pizza>> callPizzas() async {
+    HttpHelper helper = HttpHelper();
+    List<Pizza> pizzas = await helper.getPizzaList();
+    return pizzas;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('JSON Arya'),
+        ),
+        body: FutureBuilder(
+            future: callPizzas(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Pizza>> snapshot) {
+              if (snapshot.hasError) {
+                return const Text('Something went wrong');
+              }
+              if (!snapshot.hasData) {
+                return const CircularProgressIndicator();
+              }
+              return ListView.builder(
+                  itemCount:
+                      (snapshot.data == null) ? 0 : snapshot.data!.length,
+                  itemBuilder: (BuildContext context, int position) {
+                    return ListTile(
+                      title: Text(snapshot.data![position].pizzaName),
+                      subtitle: Text(snapshot.data![position].description +
+                          '- € ' +
+                          snapshot.data![position].price.toString()),
+                    );
+                  });
+            }));
+  }
+}
